18dd51d839b631324cd105cbe5ef450914055f46
Timur <timk01@mail.ru>
2026-03-03 20:26:24 +0300

    v2: добавлено базовое (по 2 объекта) начальное заполнение карты

--- src/main/java/v2/Main2.java
+++ src/main/java/v2/Main2.java
@@ -1,5 +1,7 @@
 package v2;
 
+import v2.actions.Action;
+import v2.actions.PopulateMapAction;
 import v2.entity.*;
 import v2.map.Location;
 import v2.map.WorldMap;
@@ -9,7 +11,7 @@ public class Main2 {
     public static void main(String[] args) {
         WorldMap worldMap = new WorldMap(10, 10);
 
-        Location locationGrass = new Location(0, 0);
+        /*Location locationGrass = new Location(0, 0);
         worldMap.tryAddEntity(locationGrass, new Grass());
 
         Location locationTree = new Location(0, 5);
@@ -22,7 +24,10 @@ public class Main2 {
         worldMap.tryAddEntity(locationHerbivore, new Herbivore());
 
         Location locationPredator = new Location(7, 7);
-        worldMap.tryAddEntity(locationPredator, new Predator());
+        worldMap.tryAddEntity(locationPredator, new Predator());*/
+
+        Action action = new PopulateMapAction();
+        action.execute(worldMap);
 
         Renderer renderer = new Renderer(worldMap);
         renderer.draw();
--- src/main/java/v2/actions/PopulateMapAction.java
+++ src/main/java/v2/actions/PopulateMapAction.java
@@ -1,12 +1,62 @@
 package v2.actions;
 
+import v2.entity.Entity;
+import v2.entity.EntityFactory;
+import v2.entity.EntityType;
+import v2.entity.Tree;
+import v2.map.Location;
 import v2.map.WorldMap;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 public class PopulateMapAction implements Action {
-    @Override
-    public void execute(WorldMap map) {
+
+    private int ROCK_COUNTER = 2;
+    private int TREE_COUNTER = 2;
+    private int GRASS_COUNTER = 2;
+    private int HERBIVORE_COUNTER = 2;
+    private int PREDATOR_COUNTER = 2;
+
+    record EntityPlan(EntityType entityType, int quantity) {
 
     }
 
+    @Override
+    public void execute(WorldMap map) {
+        EntityFactory entityFactory = new EntityFactory();
 
+        List<Location> emptyLocations = fillEmptyLocationsList(map);
+        Collections.shuffle(emptyLocations);
+
+        List<EntityPlan> planList = new ArrayList<>();
+        for (EntityType entityType : EntityType.values()) {
+            planList.add(new EntityPlan(entityType, 2));
+            //toDo 2 - приходит из общего конфига как каунтер существ (максимальный)
+        }
+
+        for (EntityType entityType : EntityType.values()) {
+            for (int i = 0; i < 2; i++) {
+            Location location = emptyLocations.get(0);
+            emptyLocations.remove(0);
+            map.tryAddEntity(location, entityFactory.createEntity(entityType));
+            }
+        }
+    }
+
+    private List<Location> fillEmptyLocationsList(WorldMap map) {
+        List<Location> freeLocations = new ArrayList<>();
+        Location location;
+        for (int y = 0; y < map.getHeight(); y++) {
+            for (int x = 0; x < map.getWidth(); x++) {
+                location = new Location(x, y);
+                if (map.getEntity(location).isEmpty()) {
+                    freeLocations.add(location);
+                }
+            }
+        }
+        return freeLocations;
+    }
 }
